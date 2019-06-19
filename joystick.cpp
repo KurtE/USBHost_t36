f32374f93fd6fdad57adbd41bf8446f7e7cbb181
Kurt Eckhardt <kurte@rockisland.com>
2019-06-19 07:02:24 -0700

    Only allow joystick object to claim one physical object at time
    If it is already talking to another Joystick it should fail...
    
    Need to expand and don't connect if it is connected in a different way as well (USB or HID...)

--- joystick.cpp
+++ joystick.cpp
@@ -738,6 +738,9 @@ void JoystickController::disconnect()
 
 bool JoystickController::claim_bluetooth(BluetoothController *driver, uint32_t bluetooth_class, uint8_t *remoteName) 
 {
+	// If we are already in use than don't grab another one. 
+	if (btdevice) return false;
+
 	if ((((bluetooth_class & 0xff00) == 0x2500) || (((bluetooth_class & 0xff00) == 0x500))) && ((bluetooth_class & 0x3C) == 0x08)) {
 		DBGPrintf("JoystickController::claim_bluetooth TRUE\n");
 		btdriver_ = driver;
